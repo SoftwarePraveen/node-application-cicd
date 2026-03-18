dccddec060208e2201ed19a582aca3d4ce1307ca
praveen <fabinisticpraveen@gmail.com>
2026-03-18 13:42:54 +0530

    new routes s

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.get("/cicd",(req,res)=>{
     return res.send("New Routes to check CI-CD")
 })
 
+app.get("/again",(req,res)=>{
+    return res.send("New Routes to check CI-CD")
+})
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
 })
